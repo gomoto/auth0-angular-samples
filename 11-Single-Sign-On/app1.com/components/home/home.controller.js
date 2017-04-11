0622ef259ded0a4147a6514d5b9ba20f5d331c2a
Ryan Gomoto <ryan@cyrusbio.com>
2017-04-10 19:09:02 -0700

    Set AUTH0_LOGOUT_URL

--- 11-Single-Sign-On/app1.com/components/home/home.controller.js
+++ 11-Single-Sign-On/app1.com/components/home/home.controller.js
@@ -19,7 +19,7 @@
 
     vm.logoutFromAuth0 = function() {
       angularAuth0.logout({
-        returnTo: 'http://localhost:3000/',
+        returnTo: AUTH0_LOGOUT_URL,
         client_id: AUTH0_CLIENT_ID
       });
       authService.logout();
